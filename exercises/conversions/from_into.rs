72d7fa39a4c51f8f791bfaefae369e05f55e25c7
arong1238 <64000750+arong1238@users.noreply.github.com>
2023-04-15 18:19:46 +0800

    Update from_into.rs

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -9,8 +9,7 @@ struct Person {
     age: usize,
 }
 
-// We implement the Default trait to use it as a fallback
-// when the provided string is not convertible into a Person object
+// 我们实现 Default trait 用于当给定的字符串无法转换为一个 Person 对象时的回退。
 impl Default for Person {
     fn default() -> Person {
         Person {
@@ -20,32 +19,52 @@ impl Default for Person {
     }
 }
 
-// Your task is to complete this implementation
-// in order for the line `let p = Person::from("Mark,20")` to compile
-// Please note that you'll need to parse the age component into a `usize`
-// with something like `"4".parse::<usize>()`. The outcome of this needs to
-// be handled appropriately.
+// 你的任务是完成这个实现
+// 以便于编译行 `let p = Person::from("Mark,20")`。
+// 请注意，你需要将 age 部分解析为一个 `usize`，方法类似于 `"4".parse::<usize>()`。
+// 它的结果需要被妥善处理。
 //
-// Steps:
-// 1. If the length of the provided string is 0, then return the default of Person
-// 2. Split the given string on the commas present in it
-// 3. Extract the first element from the split operation and use it as the name
-// 4. If the name is empty, then return the default of Person
-// 5. Extract the other element from the split operation and parse it into a `usize` as the age
-// If while parsing the age, something goes wrong, then return the default of Person
-// Otherwise, then return an instantiated Person object with the results
+// 步骤：
+// 1. 如果提供的字符串长度为 0，则返回默认的 Person
+// 2. 在字符串的逗号处分离
+// 3. 提取分离结果的第一部分作为 name
+// 4. 如果 name 是空的，返回默认的 Person
+// 5. 提取分离结果的另一部分并将其解析为一个 `usize` 作为 age
+// 如果当解析 age 时发生了错误，那么返回默认版本的 Person
+// 不然， 利用结果返回一个实例化的 Person 对象
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Default::default()
+        } else {
+            if let Some((name, age)) = s.split_once(',') {
+                if name.is_empty() {
+                    Default::default()
+                } else {
+                    match age.parse::<usize>() {
+                        Ok(age) => {
+                            Person {
+                                name: String::from(name),
+                                age
+                            }
+                        }
+                        _ => Default::default()
+                    }
+                }
+            } else {
+                Default::default()
+            }
+        }
     }
 }
 
 fn main() {
-    // Use the `from` function
+    // 使用 `from` 函数
     let p1 = Person::from("Mark,20");
-    // Since From is implemented for Person, we should be able to use Into
+    // 由于 From 被 Person 实现了，我们应该可以使用 Into
     let p2: Person = "Gerald,70".into();
     println!("{:?}", p1);
     println!("{:?}", p2);
@@ -132,3 +151,4 @@ mod tests {
         assert_eq!(p.age, 30);
     }
 }
+
