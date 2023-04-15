fc2dba259760d5018441fbb2c9ae200c8cdac790
arong1238 <64000750+arong1238@users.noreply.github.com>
2023-04-15 18:47:38 +0800

    Update structs3.rs

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -4,7 +4,7 @@
 // Make the code compile and the tests pass!
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 struct Package {
@@ -26,12 +26,14 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        // 这边有东西...
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        // 这边有东西...
+        self.weight_in_grams * cents_per_gram
     }
 }
 
@@ -81,3 +83,4 @@ mod tests {
         assert_eq!(package.get_fees(cents_per_gram * 2), 9000);
     }
 }
+
