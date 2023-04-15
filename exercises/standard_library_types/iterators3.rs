ea7f3578c60a363f5d7648bc3dbb46b2d5125a02
arong1238 <64000750+arong1238@users.noreply.github.com>
2023-04-15 18:44:06 +0800

    Update iterators3.rs

--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -6,7 +6,7 @@
 //    list_of_results functions.
 // Execute `rustlings hint iterators3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum DivisionError {
@@ -20,24 +20,30 @@ pub struct NotDivisibleError {
     divisor: i32,
 }
 
-// Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
-// Otherwise, return a suitable error.
+// 如果 'a' 可被 'b' 整除，计算 'a' 除以 'b'。
+// 不然，返回适当的错误。
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
-    todo!();
+    if b == 0 {
+        Err(DivisionError::DivideByZero)
+    } else if a % b != 0 {
+        Err(DivisionError::NotDivisible(NotDivisibleError { dividend: a, divisor: b}))
+    } else {
+        Ok(a / b)
+    }
 }
 
-// Complete the function and return a value of the correct type so the test passes.
-// Desired output: Ok([1, 11, 1426, 3])
-fn result_with_list() -> () {
+// 完成函数并返回正确类型的值让测试通过。
+// 期待的输出： Ok([1, 11, 1426, 3])
+fn result_with_list() -> Result<Vec<i32>, DivisionError> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    numbers.into_iter().map(|n| divide(n, 27)).collect()
 }
 
-// Complete the function and return a value of the correct type so the test passes.
-// Desired output: [Ok(1), Ok(11), Ok(1426), Ok(3)]
-fn list_of_results() -> () {
+// 完成函数并返回正确类型的值让测试通过。
+// 期待的输出： [Ok(1), Ok(11), Ok(1426), Ok(3)]
+fn list_of_results() -> Vec<Result<i32, DivisionError>> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    numbers.into_iter().map(|n| divide(n, 27)).collect()
 }
 
 #[cfg(test)]
@@ -83,3 +89,4 @@ mod tests {
         );
     }
 }
+
