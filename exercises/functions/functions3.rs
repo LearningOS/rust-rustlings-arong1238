e501c90e4ecdd8009922f9a6ac6f64f51681b1b1
arong1238 <64000750+arong1238@users.noreply.github.com>
2023-04-15 18:28:15 +0800

    Update functions3.rs

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -1,27 +1,14 @@
-// functions4.rs
-// Execute `rustlings hint functions4` or use the `hint` watch subcommand for a hint.
-
-// This store is having a sale where if the price is an even number, you get
-// 10 Rustbucks off, but if it's an odd number, it's 3 Rustbucks off.
-// (Don't worry about the function bodies themselves, we're only interested
-// in the signatures for now. If anything, this is a good way to peek ahead
-// to future exercises!)
+// functions3.rs
+// Execute `rustlings hint functions3` or use the `hint` watch subcommand for a hint.
 
 
 
 fn main() {
-    let original_price = 51;
-    println!("Your sale price is {}", sale_price(original_price));
+    call_me(4);
 }
 
-fn sale_price(price: i32) -> i32 {
-    if is_even(price) {
-        price - 10
-    } else {
-        price - 3
+fn call_me(num: u32) {
+    for i in 0..num {
+        println!("Ring! Call number {}", i + 1);
     }
-}
-
-fn is_even(num: i32) -> bool {
-    num % 2 == 0
 }
